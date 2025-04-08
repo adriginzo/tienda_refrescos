53c072e260d2803872f9f6ac3ab3134ccbdfa097
adriginzo <adri@ginzocloud.es>
2025-04-08 19:16:09 +0200

    starting with the frontend

--- microservicio-compras/frontend/src/app/components/compras-list/compras-list.component.ts
+++ microservicio-compras/frontend/src/app/components/compras-list/compras-list.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CompraService } from '../../services/compras.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-compras-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './compras-list.component.html',
-  styleUrl: './compras-list.component.css'
+  styleUrls: ['./compras-list.component.css']
 })
-export class ComprasListComponent {
+export class ComprasListComponent implements OnInit {
 
+  compras: any[] = [];
+  isLoading = false;
+  error: string | null = null;
+
+  constructor(private compraService: CompraService) {}
+
+  ngOnInit(): void {
+    this.loadCompras();
+  }
+
+  loadCompras(): void {
+    this.isLoading = true;
+    this.error = null;
+
+    this.compraService.getCompras().subscribe(
+      (data) => {
+        this.compras = data;
+        this.isLoading = false;
+      },
+    );
+  }
 }
--- microservicio-compras/frontend/src/app/services/compras.service.ts
+++ microservicio-compras/frontend/src/app/services/compras.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ComprasService {
+export class CompraService {
+  private apiUrl = 'http://localhost:3002/api/compras';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCompras(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+
+  getComprasById(id: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
+
 }
